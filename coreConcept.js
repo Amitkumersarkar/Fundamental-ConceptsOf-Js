93fd4f9f7f2cb0e6e79999b5da6830fc54a58291
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-01-22 23:56:57 +0600

    added new file

---
+++ coreConcept.js
@@ -0,0 +1,16 @@
+// length, push
+
+const numbers = [89, 30, 20, 10];
+numbers[0] = 50;
+
+// for loop
+for (let i = 0; i < numbers.length; i++) {
+    const numbers = numbers[i];
+    console.log(numbers);
+}
+
+// function 
+function multiply(num1, num2) {
+    const result = num1 * num2;
+    return result;
+}
