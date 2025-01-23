1f5f0c04e270cc04d3f67f1eb0966a70d49e7db0
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-01-24 02:15:02 +0600

    added forEach method

--- array-mathod.js
+++ array-mathod.js
@@ -8,3 +8,10 @@ const products = [
 
 const brands = products.map(product => product.brand);
 console.log(brands);
+const prices = products.map(product => product.price);
+console.log(prices);
+
+// forEach used if something not returnable
+
+products.forEach(product => console.log(product));
+products.forEach(product => console.log(product.brand));
