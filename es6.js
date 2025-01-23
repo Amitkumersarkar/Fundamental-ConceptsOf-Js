bc61ffa42f7dfc3449bf1275432346176225948c
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-01-24 01:18:51 +0600

    added multi para arrow function

--- es6.js
+++ es6.js
@@ -9,3 +9,11 @@ const student = {
 const about = `my name is ${student.name} 
 age of ${student.age} has number ${numbers[2]} also liked movies ${student.movies[0]}`;
 console.log(about);
+
+// arrow function
+
+const getFiftyFive = () => 55;
+const addSixtyFive = num => num + 65;
+const isEven = x => x % 2 == 0;
+// multi parameter arrow function 
+const addThree = (x, y, z) => x + y + z;
